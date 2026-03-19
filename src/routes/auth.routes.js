161f11516036750ae6a2aaaae55420579b704775
deepak-dev23 <ds1090867@gmail.com>
2026-03-19 12:46:49 +0530

    all route tested

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,7 +5,7 @@ import { verifyToken } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.post('/register', registerUser);
-router.post('/login', verifyToken, loginUser);
+router.post('/login', loginUser);
 router.post("/logout", verifyToken, logoutUser)
 
 
